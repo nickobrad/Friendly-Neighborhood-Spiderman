e21104e3c738e936291bf7cd3c5d5d731e9a9e14
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-09-22 00:39:35 +0300

    apps created

---
+++ spider/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class SpiderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'spider'
+
+    def ready(self):
+        import spider.signals
