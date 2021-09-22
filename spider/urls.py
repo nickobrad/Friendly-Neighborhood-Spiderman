42d7e28a8acde6c72368eaa3db4d86dda8d9d095
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-09-23 00:14:32 +0300

    urls updated

--- spider/urls.py
+++ spider/urls.py
@@ -4,7 +4,7 @@ from django.urls.conf import include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import UpdateBusiness, register, loginuser, home, logout_view, my_profile, UpdateProfile
+from .views import UpdateBusiness, register, loginuser, home, logout_view, my_profile, search_results,UpdateProfile
 
 urlpatterns = [
     path('register/', register, name = 'register'),  
@@ -13,6 +13,7 @@ urlpatterns = [
     path('profile', my_profile, name = 'profile'),
     path('profile/update/<int:pk>', UpdateProfile.as_view(), name = 'updateprofile'),
     path('business/update/<int:pk>', UpdateBusiness.as_view(), name = 'updatebusiness'),
+    path('search/', search_results, name = 'search_business'),
     path('logout/', logout_view, name = 'logout'),
 ]
 if settings.DEBUG:
